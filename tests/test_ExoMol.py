e2d4c112c0abcc92f00d74769456202769826b35
arnav_agrawal <aa687@cornell.edu>
2022-04-19 12:23:01 -0400

    Started to create unit testing files in new folder called 'tests'

---
+++ tests/test_ExoMol.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 12:03:57 2022
+
+@author: arnav
+"""
+
+from excalibur import ExoMol
+
+
+def test_check():
+    
+def test_get_default_iso():
+    
+def test_get_default_linelist():
+    
+def test_determine_linelist():
+    
+def test_process_files(input_dir):
+    
+def test_summon_ExoMol(molecule, isotopologue, line_list, URL):
+    
+def test_load_states(input_directory):
