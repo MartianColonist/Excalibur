d32f010e07017a7268e962c9b58caca59c5dc5ad
Ryan MacDonald <rmacdonald@astro.cornell.edu>
2022-06-22 12:48:02 -0400

    Added option for user to set x and y ranges

--- excalibur/plot.py
+++ excalibur/plot.py
@@ -8,7 +8,9 @@ from matplotlib.ticker import MultipleLocator, AutoLocator, FormatStrFormatter, 
 
 
 def plot_sigma_wl(species, temperature, log_pressure, nu_arr = [], sigma_arr = [], 
-                  file = '', database = '', plot_dir = './plots/', ionization = 1, **kwargs):
+                  file = '', database = '', plot_dir = './plots/', ionization = 1, 
+                  wl_min = None, wl_max = None, sigma_min = None, sigma_max = None, 
+                  **kwargs):
     """
     Generate a plot of a cross_section file, in both wavelength and wavenumber
 
@@ -34,30 +36,6 @@ def plot_sigma_wl(species, temperature, log_pressure, nu_arr = [], sigma_arr = [
     None.
 
     """
-    
-    # User did not specify a wl and sigma array and also did not specify a file
-  #  if nu_arr == [] and sigma_arr == [] and file == '':
-  #      print("----- You have not specified enough arguments for this function. ----- ")
-  #      sys.exit(0)
-     
-    # User specified a file    
-  #  if file != '':
-  #      try:
-  #          data = pd.read_fwf(file, widths = [13, 14], names = ['Nu Out', 'Sigma Out'], header = None)
-  #          nu = data['Nu Out']
-  #          sigma = data['Sigma Out']
-    
-  #          wl = 1.0e4/nu
-            
-  #      except TypeError:
-  #          print("----- You did not pass in a valid file. ----- ")
-  #          sys.exit(0)
-    
-    # User passed in wl and sigma arr        
- #   if nu_arr != [] and sigma_arr != []:
- #       nu = nu_arr
- #       sigma = sigma_arr
- #       wl = 1.0e4/nu
         
     if not os.path.exists('./plots/'):
         os.mkdir('./plots')
@@ -66,7 +44,19 @@ def plot_sigma_wl(species, temperature, log_pressure, nu_arr = [], sigma_arr = [
     nu_plt = nu_arr
     wl_plt = 1.0e4/nu_plt
     sigma_plt = sigma_arr + 1.0e-250   # Add small value to avoid log(0) on log plots
-    
+
+    # Set x range
+    if (wl_min == None):
+        wl_min = np.min(wl_plt)
+    if (wl_max == None):
+        wl_max = np.max(wl_plt)
+
+    # Set y range
+    if (sigma_min == None):
+        sigma_min = 1.0e-30
+    if (sigma_max == None):
+        sigma_max = 10.0**(np.max(np.ceil(np.log10(sigma_plt) / 2.0) * 2.0))
+
     # If user provided a list or numpy array for log_P and T, convert into float
     log_pressure = np.asarray(log_pressure).item()
     temperature = np.asarray(temperature).item()
@@ -107,22 +97,18 @@ def plot_sigma_wl(species, temperature, log_pressure, nu_arr = [], sigma_arr = [
     fig = plt.figure()
     ax = plt.gca()
     
-    min_sigma = 1.0e-30
-    max_sigma = 10.0**(np.max(np.ceil(np.log10(sigma_plt) / 2.0) * 2.0))
-    
     ax.loglog(wl_plt, sigma_plt, lw=0.3, alpha = 0.5, color= 'crimson', label = (species + ion + r' Cross Section')) 
     
     ax.set_xticks([0.2, 0.4, 0.6, 0.8, 1.0, 2.0, 4.0, 6.0, 8.0, 10.0])
     ax.set_xticklabels(['0.2', '0.4', '0.6', '0.8', '1', '2', '4', '6', '8', '10'])
     
-    ax.set_ylim([min_sigma, max_sigma])
- #   ax.set_ylim([1.0e-30, round(np.log10(np.max(sigma)) + 2.0)])
-    ax.set_xlim([0.2, 10.0])
+    ax.set_ylim([sigma_min, sigma_max])
+    ax.set_xlim([wl_min, wl_max])
     
     ax.set_ylabel(r'Cross Section (cm$^2$)', size = 14)
     ax.set_xlabel(r'Wavelength (μm)', size = 14)
     
-    ax.text(0.22, 10**(np.log10(max_sigma) - 1.0), (r'T = ' + str(temperature) + r' K, P = ' + str(pressure) + r' bar'), fontsize = 10)
+    ax.text(0.22, 10**(np.log10(sigma_max) - 1.0), (r'T = ' + str(temperature) + r' K, P = ' + str(pressure) + r' bar'), fontsize = 10)
     
     legend = plt.legend(loc='upper right', shadow=False, frameon=False, prop={'size':10})
     
