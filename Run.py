f4553148428780f74f7a9ab28f1ecdf08010bc3b
arnav_agrawal <aa687@cornell.edu>
2022-07-25 19:23:38 +0530

    Adjusted plotting routine to be able to plot multiple cross sections at once.

--- Run.py
+++ Run.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from excalibur.core import summon
 from excalibur.core import compute_cross_section
-from excalibur.plot import plot_sigma_wl
+from excalibur.plot import plot_spectra, spectra_collection
 
 # Parameters
 
@@ -15,6 +15,7 @@ database = 'exomol'
 P = 1e-3       # Pressure (bar)
 T = 2000       # Temperature (K)
 
+
 # Download line list
 summon(species = species, database = database, 
        isotope = isotope, VALD_data_dir = './VALD Line Lists/')
@@ -24,9 +25,42 @@ summon(species = species, database = database,
 nu, sigma = compute_cross_section(input_dir = './input/', database = database, 
                                   species = species, log_pressure = np.log10(P), 
                                   temperature = T, isotope = isotope,
-                                  N_cores = 1, nu_out_min = 200, nu_out_max = 50000, dnu_out = 0.01)
+                                  N_cores = 1, nu_out_min = 5000, nu_out_max = 50000, dnu_out = 0.01)
+
+# Download line list
+summon(species = 'NaCl', database = database, 
+       isotope = isotope, VALD_data_dir = './VALD Line Lists/')
+
+# Create cross section
+nu2, sigma2 = compute_cross_section(input_dir = './input/', database = database, 
+                                  species = 'NaCl', log_pressure = np.log10(P), 
+                                  temperature = T, isotope = isotope,
+                                  N_cores = 1, nu_out_min = 200, nu_out_max = 5000, dnu_out = 0.01)
+
+spectra = spectra_collection(nu, sigma, collection = [])
+
+
+spectra = spectra_collection(nu2, sigma2, collection = spectra)
+
 
 
 # Plot cross section
-plot_sigma_wl(nu_arr = nu, sigma_arr = sigma, species = species, temperature = T, 
-              log_pressure = np.log10(P), database = database, plot_dir = './plots/')
+spectrum = plot_spectra(spectra, ['FeO', 'NaCl'], 'test')
+
+
+'''
+spectra = spectra_collection(nu, sigma, collection = [])
+
+# Download line list
+summon(species = 'NaCl', database = database, 
+       isotope = isotope, VALD_data_dir = './VALD Line Lists/')
+
+
+# Create cross section
+nu, sigma2 = compute_cross_section(input_dir = './input/', database = database, 
+                                  species = 'NaCl', log_pressure = np.log10(P), 
+                                  temperature = T, isotope = isotope,
+                                  N_cores = 1, nu_out_min = 200, nu_out_max = 50000, dnu_out = 0.01)
+
+spectra = spectra_collection(nu, sigma2)
+'''
