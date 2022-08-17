e6f748633aca97dd731c75c9581062fa179cef4c
arnav_agrawal <aa687@cornell.edu>
2022-08-17 15:41:09 -0400

    More changes.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import mock
 #MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 #for mod_name in MOCK_MODULES:
     #sys.modules[mod_name] = mock.Mock()
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -45,7 +45,7 @@ extensions = ['sphinx_rtd_theme',
     'autoapi.extension']
 
 #autoapi_type = 'python'
-autoapi_dirs = ['../../excalibur']
+autoapi_dirs = ['../excalibur']
 autoapi_ignore = ["*/hapi*.py"]
 
 # Add any paths that contain templates here, relative to this directory.
