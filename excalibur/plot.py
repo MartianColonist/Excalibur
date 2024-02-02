d49c33f5dfa8697a39bd7b2fe87163e81f78049e
arnav_agrawal <aa687@cornell.edu>
2024-02-02 15:13:55 -0500

    change to run.py for hitran/exomol testing
    also a minor change to the plotting routine to incorporate some modules
    arnav had imported before but were gone with Ryan's changes.

--- excalibur/plot.py
+++ excalibur/plot.py
@@ -9,6 +9,13 @@ from scipy.ndimage import gaussian_filter1d
 
 from excalibur.misc import round_sig_figs
 
+from .hapi import isotopologueName
+import excalibur.HITRAN as HITRAN
+import excalibur.ExoMol as ExoMol
+import sys
+import csv
+import re
+
 plt.style.use('classic')
 plt.rc('font', family = 'serif')
 matplotlib.rcParams['svg.fonttype'] = 'none'
@@ -440,3 +447,141 @@ def compare_cross_sections(molecule, label_1, label_2, nu_arr_1 = [], nu_arr_2 =
         plt.savefig('./plots/' + molecule + '_comparison_' + label_1 + '_' + label_2 + '_zoom_a0.pdf')
 
     print("\nPlotting complete.")
+
+
+def read_cross_section_file(species, database, filename, isotope = 'default',
+                            ionization_state = 1, linelist = 'default', output_dir = './output/'):
+
+    """
+    Read in a previously computed cross section on a user's machine, and return the 
+    wavenumber and absorption cross section columns.
+
+    Parameters
+    ----------
+    species : String
+        Molecule for which the cross section was computed.
+    database : String
+        Database the line list was downloaded from.
+    filename : String
+        Full file name of the cross section file(eg. 'this_is_my_file.txt').
+    isotope : String
+        Isotopologue of the molecule for which the cross-section is to be created. The default is 'default'.
+    ionization_state : int
+        Ionization state, in case of an atomic species. The default is 1. 
+    linelist : String
+        Line list that is being used. HITRAN/HITEMP/VALD used as the line list name for these
+        databases respectively. ExoMol has its own named line lists. The default is 'default'. 
+    output_dir = String
+        'Prefix' of the directory containing the cross section files. If the files were downloaded
+        using our script with no modifications, output_dir will end in '/output'. The default is './output/'. 
+
+    Returns
+    -------
+    nu : numpy array
+        Wavenumber column of cross section file.
+    sigma : numpy array
+        Absorption cross section column of cross section file.
+
+    """
+    database = database.lower()
+
+    if database in ['hitran', 'hitemp']:
+        molecule_dict = HITRAN.create_id_dict()
+        mol_id = molecule_dict.get(species)
+        if isotope == 'default':
+            isotope = isotopologueName(mol_id, 1)
+        else:
+            isotope = isotopologueName(mol_id, isotope)
+        isotope = HITRAN.replace_iso_name(isotope)
+
+    if database == 'exomol':
+        if isotope == 'default':
+            isotope = ExoMol.get_default_iso(species)
+
+    if database == 'vald':
+        ion_roman = ''
+        for i in range(ionization_state):
+            ion_roman += 'I'
+
+
+    if linelist == 'default':
+        if database == 'exomol':
+            temp_isotope = re.sub('[(]|[)]', '', isotope)
+            linelist = ExoMol.get_default_linelist(species, temp_isotope)
+        if database == 'hitran':
+            linelist = 'HITRAN'
+        if database == 'hitemp':
+            linelist = 'HITEMP'
+        if database == 'vald':
+            linelist = 'VALD'
+
+    if database == 'vald':
+        tag = '(' + ion_roman + ')'
+    else:
+        tag = isotope
+
+    if (database == 'exomol'):
+        output_directory = (output_dir + species + '  ~  (' + tag + ')/' +
+                           'ExoMol' + '/' + linelist + '/')
+    else:
+        output_directory = (output_dir + species + '  ~  ' + tag + '/' +
+                           linelist + '/')
+
+    if os.path.exists(output_directory):
+        file  = output_directory + filename
+        with open(file, 'r') as f:
+            contents = csv.reader(f, delimiter=' ')
+            nu = []
+            sigma = []
+            for cols in contents:
+                nu.append(float(cols[0]))
+                sigma.append(float(cols[1]))
+
+        nu = np.asarray(nu)
+        sigma = np.asarray(sigma)
+        return  nu, sigma
+
+    else:
+        print("You don't seem to have a local folder with the parameters you entered.\n")
+
+        if not os.path.exists(output_dir + '/'):
+            print("----- You entered an invalid output directory into the cross_section() function. Please try again. -----")
+            sys.exit(0)
+
+        elif not os.path.exists(output_dir + '/' + species + '  ~  (' + tag + ')/'):
+            print("----- There was an error with the molecule + isotope you entered. Here are the available options: -----\n")
+            for folder in os.listdir(output_dir + '/'):
+                if not folder.startswith('.'):
+                    print(folder)
+            sys.exit(0)
+
+        else:
+            print("There was an error with the line list. These are the linelists available: \n")
+            for folder in os.listdir(output_dir + '/' + species + '  ~  (' + tag + ')/'):
+                if not folder.startswith('.'):
+                    print(folder)
+            sys.exit(0)
+
+def cross_section_collection(new_x, new_y, collection = []):
+    '''
+    Add new cross section (that is, wavelength and absorption cross section) to the collection
+
+    Parameters
+    ----------
+    new_x : list
+        DESCRIPTION.
+    new_y : list
+        DESCRIPTION.
+    collection : 2d list, optional
+        DESCRIPTION. The default is [].
+
+    Returns
+    -------
+    collection : TYPE
+        DESCRIPTION.
+
+    '''
+
+    collection.append([new_x, new_y])
+
+    return collection
