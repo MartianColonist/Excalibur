9cd25adf6f2a3331585f5b6743e3186ccd291078
Ryan MacDonald <rmacdonald@astro.cornell.edu>
2024-07-18 16:23:30 -0700

    Update default NH linelist

--- Excalibur/ExoMol.py
+++ Excalibur/ExoMol.py
@@ -118,7 +118,7 @@ def get_default_linelist(molecule, isotopologue):
     default_list = {'H2(1H2)': 'RACPPK', 'N2(14N2)': 'WCCRMT', 'C2(12C2)': '8states', 
                     'CO(12C-16O)': 'Li2015', 'NO(14N-16O)': 'XABC', 'PO(31P-16O)': 'POPS',
                     'VO(51V-16O)': 'VOMYT', 'YO(89Y-16O)': 'SSYT', 'CN(12C-14N)': 'MoLLIST',
-                    'NH(14N-1H)': 'MoLLIST', 'CH(12C-1H)': 'MoLLIST', 'OH(16O-1H)': 'MoLLIST',
+                    'NH(14N-1H)': 'kNigHt', 'CH(12C-1H)': 'MoLLIST', 'OH(16O-1H)': 'MoLLIST',
                     'SH(32S-1H)': 'GYT', 'HF(1H-19F)': 'Coxon-Hajig', 'CS(12C-32S)': 'JnK', 
                     'NS(14N-32S)': 'SNaSH', 'PS(31P-32S)': 'POPS', 'PH(31P-1H)': 'LaTY', 
                     'PN(31P-14N)': 'YYLT', 'CP(12C-31P)': 'MoLLIST', 'H2_p(1H-2H_p)': 'ADJSAAM', 
